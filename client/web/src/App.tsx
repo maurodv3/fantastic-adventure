46aecdef0bedf2514cdae93df3386a1497f63087
Mauro Vidal <mauro_33_14@hotmail.com>
2021-01-18 17:11:31 -0300

    Add React client skeleton with build config.

---
+++ client/web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Cell from "./components/Cell";
+
+function App() {
+  return (
+    <div className="App">
+        <Cell/>
+        <Cell/>
+        <Cell/>
+        <Cell/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/web/src/components/Cell.tsx
@@ -0,0 +1,11 @@
+import './Cell.css';
+
+function Cell() {
+    return (
+        <div className="Cell">
+            <p>*</p>
+        </div>
+    );
+}
+
+export default Cell;
